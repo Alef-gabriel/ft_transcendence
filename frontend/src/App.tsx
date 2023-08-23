e425b713e758e04944d44bc4856ddf34bdc677ba
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro.local>
2023-08-23 17:06:29 -0300

    Starting Front End development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import PongGame from './PongGame';
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header.js'
+import Home from './pages/Home.js'
+import Profile from './pages/Profile.js'
+import Login from './pages/Login.js'
+import Register from './pages/Register.js'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    <Router>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Link, NavigateFunction, useNavigate } from 'react-router-dom'
+
+const Header = () => {
+    const navigate: NavigateFunction = useNavigate()
+
+    const logoutClick = () => {
+        navigate('/login')
+    }
+
+  return (
+    <div className ="header">
+        <div>
+            <Link id="header-logo" to="/">LOGO</Link>
+        </div>
+
+        <div className="links--wrapper">
+            <>
+                <Link to="/" className="header--link">Home</Link>
+                <Link to="/profile" className="header--link">Profile</Link>
+                <button onClick={logoutClick} className="btn">Logout</button>
+            </>
+            {/* <>
+
+                <Link className="btn" to="/login">Login</Link>
+            </> */}
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
