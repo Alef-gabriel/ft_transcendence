a554ec636597c01841c6c4bbef656cceef28dc8b
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro.local>
2023-09-15 13:16:59 -0300

    Displaying profile on home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,33 @@
 import { useProfile } from "../../utils/ProfileContext.tsx";
 import { ProfileContextData } from "../../utils/interfaces/ProfileContextData.ts";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useThrowAsyncError from "../../utils/hooks/useThrowAsyncError.ts";
 
+//TODO: Trocar o avatar por uma imagem do backend
 const Home = () => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
   const { profile} = useProfile() as ProfileContextData;
+  const throwAsyncError = useThrowAsyncError();
+
+  useEffect(() => {
+    // Replace with the actual backend endpoint to fetch the image
+
+    axios
+      .get('http://localhost:3000/api/profile/avatar/12', {
+        responseType: 'blob',
+        withCredentials: true,
+      })
+      .then((response) => {
+        const blob = new Blob([response.data], { type: response.headers['content-type'] });
+        const imageUrl = URL.createObjectURL(blob);
+        setImageSrc(imageUrl);
+      })
+      .catch((error) => {
+        console.error('Error fetching image:', error);
+        throwAsyncError(error);
+      });
+  }, [throwAsyncError]);
 
   return (
 
@@ -10,20 +35,15 @@ const Home = () => {
       <h1>Player Dashboard</h1>
 
       <br></br>
-      <br></br>
-
-      <p>AvatarId: {profile?.avatarId}</p>
-      <br></br>
-      <p>Nickname: {profile?.nickname}</p>
+      <div>
+        {imageSrc && <img src={imageSrc} alt="Image" />}
+        <p>Nickname: {profile?.nickname}</p>
+      </div>
       <br></br>
       <p>Wins: {profile?.wins}</p>
-      <br></br>
       <p>Draws: {profile?.draws}</p>
-      <br></br>
       <p>Loses: {profile?.losses}</p>
-      <br></br>
-      <p>ranking: Not Implement Yet</p>
-
+      <p>Ranking: Not Implement Yet</p>
     </div>
 
   );
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,6 +4,8 @@ import { NavigateFunction, useNavigate } from "react-router-dom";
 import { useProfile } from "../../utils/ProfileContext.tsx";
 import { ProfileContextData } from "../../utils/interfaces/ProfileContextData.ts";
 
+
+//TODO: Trocar o avatar por uma imagem do backend
 const Profile = () => {
   const navigate: NavigateFunction = useNavigate();
   const { user, disable2FA } = useAuth() as AuthContextData;
@@ -23,11 +25,11 @@ const Profile = () => {
 
       <br></br>
 
-      <button className="btn">Change Avatar</button>
+      <button className="btn">Change Avatar (Not Impl)</button>
 
       <br></br>
 
-      <button className="btn">Change Nickname</button>
+      <button className="btn">Change Nickname (Not Impl)</button>
 
 
       <br></br>
@@ -38,7 +40,7 @@ const Profile = () => {
 
       <br></br>
 
-      <button className="btn">Delete Account</button>
+      <button className="btn">Delete Account (Not Impl)</button>
 
     </div>
   );
